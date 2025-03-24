1ba13b9b6a984af19e1fa57db811eb21472ff5a3
dzx <dzxdetianxuan@outlook.com>
2025-03-24 15:57:50 +0800

    5.2优化了侧边栏选项上下间距，并使字体颜色变淡以更符合实际情况

--- src/components/CategorizationItem.jsx
+++ src/components/CategorizationItem.jsx
@@ -1,7 +1,7 @@
 const CategorizatonItem = (props) => {
     const {name} = props
     return (
-        <button className="p-1 text-sm sm:text-lg md:text-lg lg:text-xl">{name}</button>
+        <button className="px-1 py-6 text-gray-600 text-sm sm:text-lg md:text-lg lg:text-xl">{name}</button>
     )
 }
 export default CategorizatonItem
